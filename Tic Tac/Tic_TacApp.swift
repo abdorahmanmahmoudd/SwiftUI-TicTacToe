082a2fbd7a3b174aaf0e13072ab361302fe6dc12
Abdelrahman <abdorahmanmahmoudd@gmail.com>
2021-10-08 12:37:38 +0200

    Initial Commit

---
+++ Tic Tac/Tic_TacApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_TacApp.swift
+//  Tic Tac
+//
+//  Created by Abdelrahman Ali on 08/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct Tic_TacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
